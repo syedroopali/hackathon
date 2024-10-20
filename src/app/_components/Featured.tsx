1812fb3c86f977c22e5c89e33e35645b79e468f1
Syed-Roop-Ali-2729 <164709030+Syed-Roop-Ali-2729@users.noreply.github.com>
2024-10-21 01:15:33 +0500

    commit

---
+++ src/app/_components/Featured.tsx
@@ -0,0 +1,64 @@
+import Image from "next/image";
+import featuredImg1 from "/public/featuredImg1.png";
+import featuredImg2 from "/public/featuredImg2.png";
+import featuredImg3 from "/public/featuredImg3.png";
+import featuredImg4 from "/public/featuredImg4.png";
+
+export default function Featured() {
+  return (
+    <section className="w-full">
+      <div className="flex items-center justify-center flex-col py-[2.5rem] bg-[#EDEDED]">
+        <div className="w-48 flex flex-col items-center justify-center  mb-6">
+          <Image src={featuredImg4} alt="featuredImg4" />
+        </div>
+        <h2 className="text-[2.125rem] font-light">
+          Apple AirPods <strong className="font-medium">Max</strong>
+        </h2>
+        <p className="text-[#909090] font-base">
+          Computational audio. Listen, it's powerful
+        </p>
+      </div>
+
+      <div className="flex items-center justify-center flex-col py-[2.5rem] bg-[#353535]">
+        <div className="w-[20.31rem] flex flex-col items-center justify-center  mb-6">
+          <Image src={featuredImg3} alt="featuredImg3" />
+        </div>
+        <h2 className="text-[2.125rem] font-light text-white">
+          Apple Vision <strong className="font-medium">Pro</strong>
+        </h2>
+        <p className="text-[#909090] font-base text-center px-4">
+          An immersive way to experience entertainment
+        </p>
+      </div>
+
+      <div className="flex items-center justify-center flex-col py-[2.5rem] bg-[#fff]">
+        <div className="w-[20.31rem] flex flex-col items-center justify-center  mb-6">
+          <Image src={featuredImg2} alt="featuredImg3" />
+        </div>
+        <h2 className="text-[2.125rem] font-light text-black">
+          Playstation <strong className="font-medium">5</strong>
+        </h2>
+        <p className="text-[#909090] font-base text-center px-4">
+          Incredibly powerful CPUs, GPUs, and an SSD with integrated I/O will
+          redefine your PlayStation experience.
+        </p>
+      </div>
+
+      <div className="flex items-center justify-center flex-col py-[2.5rem] bg-[#EDEDED]">
+        <div className="w-[16.9375rem] flex flex-col items-center justify-center  mb-6">
+          <Image src={featuredImg1} alt="featuredImg3" />
+        </div>
+        <h2 className="text-[2.125rem] font-light text-black">
+          <strong className="font-medium">Macbook </strong>Air
+        </h2>
+        <p className="text-[#909090] font-base text-center px-4 mb-4  ">
+          The new 15‑inch MacBook Air makes room for more of what you love with
+          a spacious Liquid Retina display.
+        </p>
+        <button className="text-base inline-block w-[21.44rem] font-medium rounded-md border-2 py-3 border-black flex items-center justify-center hover:bg-black hover:text-white transition-all">
+          Shop Now
+        </button>
+      </div>
+    </section>
+  );
+}
